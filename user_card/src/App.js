81d1b30e9ae737fab2aaf0d05609f09adeea69ae
Hunter Stevens <hunterstevens1996@gmail.com>
2020-04-07 15:00:30 -0600

    fetched and displayed the login and image id for the main person and followers

---
+++ user_card/src/App.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import axios from 'axios';
+import './App.css';
+
+class App extends React.Component 
+{
+  constructor(){
+    console.log("constructor execute")
+    super();
+    this.state={
+      user:[],
+      friends:[]
+    };
+  }
+
+  componentDidMount(){
+    console.log("ComponentDidMount execute");
+    axios.get('https://api.github.com/users/HunterStevens')
+    .then(res =>{
+      console.log("fetched api: ", res);
+      this.setState({
+        user:res.data
+      });
+      console.log("new User state: ",this.state.user)
+    })
+    .catch(err =>{
+      console.log("ERROR: ", err)
+    })
+
+  }
+
+  componentDidUpdate(){
+    console.log("ComponentDidUpdate execute");
+    axios.get(`${this.state.user.followers_url}`)
+    .then(res=>{
+      console.log("Followers Api grabbed", res)
+      this.setState({
+        friends:res.data
+      })
+      console.log("new Friends state: ",this.state.friends)
+    })
+    .catch(err=>{
+      console.log("ERROR: ",err)
+    })
+  }
+
+  componentWillUnmount(){
+    console.log("ComponentWillUnMount execute");
+  }
+
+  render(){
+    console.log("render execute")
+      return (
+        <div className="App">
+        <header className="App-header">
+      <h1> {this.state.user.login}</h1>
+      <img src={this.state.user.avatar_url}/>
+      <h3>{this.state.user.url}</h3>
+        </header>
+
+        <h1>Followers</h1>
+        {this.state.friends.map(peeps=>{
+         return(
+         <div>{peeps.login}
+         <img src={peeps.avatar_url} width='200px'/>
+         </div>
+         ) 
+        })}
+      </div>
+    );
+  }
+}
+
+export default App;
